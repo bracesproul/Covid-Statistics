e39df7b09bdfccf58859fbafb111f0a81682ebb4
Brace Sproul <braceasproul@gmail.com>
2022-01-16 09:05:11 +0000

    initial commit for dynamic 'get more data' page

--- pages/[id].js
+++ pages/[id].js
@@ -1,1 +1,20 @@
-import React, { useState, useEffect } from 'react';
+export default function Home() {
+    return (
+        <div>
+            <h1>Covid Stats</h1>
+            <h3>This page will display the broken down stats and more in depth stats for each country, with the ability to change the date, if us you can change the state and possibly city. </h3>
+            <p>Data shown will comprise of</p>
+            <p>date: '2022-01-14',</p>
+            <p>confirmed: NUMBER,</p>
+            <p>deaths: NUMBER,</p>
+            <p>recovered: NUMBER,</p>
+            <p>confirmed_diff: NUMBER,</p>
+            <p>deaths_diff: NUMBER,</p>
+            <p>recovered_diff: NUMBER,</p>
+            <p>last_update: '2022-01-15 04:21:07',</p>
+            <p>active: NUMBER,</p>
+            <p>active_diff: NUMBER,</p>
+            <p>fatality_rate: NUMBER,</p>
+        </div>
+    )
+}
