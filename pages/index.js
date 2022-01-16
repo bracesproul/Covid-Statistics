397fda54bb74fab811f5effc0e0d405357d8659a
Brace Sproul <braceasproul@gmail.com>
2022-01-16 09:04:40 +0000

    compleetly redone api calls (with best practice using getStaticProps) also switched to using props to pass data to component. Also found new API provider to provide data

--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,113 @@
-import React, { useState, useEffect } from 'react';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link'
+const axios = require('axios');
 
-export default function Home() {
+export default function Home({ data }) {
   return (
-    <div>
-      <h1>Covid Stats</h1>
-      <hr />
-      <GetWorldwideData />
+    <div className={styles.Home}>
+      <HeaderBar />
+      <ActiveCases data={data} />
     </div>
   )
 }
 
+export const getStaticProps = async () => {
+  const options = {
+      method: 'GET',
+      url: 'https://covid-19-statistics.p.rapidapi.com/regions',
+      headers: {
+        'x-rapidapi-host': 'covid-19-statistics.p.rapidapi.com',
+        'x-rapidapi-key': process.env.RAPID_API_KEY
+      }
+  };
+  const result = await axios.request(options);
+  const dataFromResult = result.data.data;
+  let data = [];
 
-const GetWorldwideData = () => {
-  const [data, setData] = useState([]);
+  await Promise.all(dataFromResult.map(async (item, index) => {
+      const options = {
+              method: 'GET',
+              url: 'https://covid-19-statistics.p.rapidapi.com/reports',
+              params: {
+                iso: item.iso,
+                region_name: item.name,
+                date: '2022-01-14'
+              },
+              headers: {
+                'x-rapidapi-host': 'covid-19-statistics.p.rapidapi.com',
+                'x-rapidapi-key': process.env.RAPID_API_KEY
+              }
+            };
+  
+          const result = await axios.request(options);
+          const dataV = result.data.data[0];
+          let region;
+  
+          if (dataV?.region === undefined) {
+              region = item.name;
+          } else {
+              region = dataV.region;
+          }
+  
+          if (dataV === undefined) return;
+  
+          const dataStructure = {
+              "country_name": item.name,
+              "cases": dataV.confirmed,
+              "deaths": dataV.deaths,
+              "recovered": dataV.recovered,
+              "active": dataV.active,
+              "fatality_rate": dataV.fatality_rate,
+              "date": dataV.date,
+              "last_update": dataV.last_update,
+              "region": region
+          }
+          
+          data = [
+              ...data,
+              { dataStructure }
+          ]
+  }));
 
-  useEffect(() => {
-    fetch('https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true')
-    .then(response => response.json())
-    .then(data => data.map((data) => {
-      setData((prev) => 
-      [...prev, 
-        {
-        country: data.country,
-        infected: data.infected,
-        deceased: data.deceased
-        }
-      ])
-    }))
-  }, []);
 
-  const DisplayData = () => {
-    return (
-      <div>
-        {data.map((data) => {
-          return (
-            <div>
-              <p>{data.country}</p>
-              <p>{data.infected}</p>
-              <p>{data.deceased}</p>
-              <hr />
-            </div>
-          )
-        })}
-      </div>
-    )
+  return {
+      props: { 
+          data,
+      }
   }
 
+}
+
+function ActiveCases({ data }) {
+  console.log('inside active cases')
   return (
-    <>
-      <DisplayData />
-    </>
+      <div className={styles.AllData}>
+          <h1 className={styles.Header}>Active Cases</h1>
+          <div>
+            { data.map((item, index) => {
+                return (
+                    <span className={styles.DataWrapper} key={index}>
+                        <p>Country: {item.dataStructure.country_name} </p>
+                        <p>Cases: {item.dataStructure.cases}</p>
+                        <p>Deaths: {item.dataStructure.deaths}</p>
+                        <p>Data from {item.dataStructure.last_update}</p>
+                        <p className={styles.LinkStyle}><Link href={{pathname: `/${item.dataStructure.country_name}`}}>Get more data</Link></p>
+                    </span>
+                )
+            }) }
+          </div>
+      </div>
   )
 }
 
+const HeaderBar = () => {
+
+  return (
+    <div className={styles.HeaderBar}>
+      <h4 className={styles.HeaderItem}><Link href={{pathname: `/About`}}>About</Link></h4>
+      <h4 className={styles.HeaderItem}><Link href={{pathname: `/More-Data`}}>More Data</Link></h4>
+      <h4 className={styles.HeaderItem}><Link href={{pathname: `/Developer-Credits`}}>{"Developer & Credits"}</Link></h4>
+    </div>
+  )
+}
 
