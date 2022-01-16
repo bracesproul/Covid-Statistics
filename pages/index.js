70d47e6ae68806a626ce60ffbf0601bcc0ddf6e3
Brace Sproul <braceasproul@gmail.com>
2022-01-16 23:27:02 +0000

    compleetly redid how I parse and display API data, changed links to route to the ISO instead of country name

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import styles from '../styles/Home.module.css';
 import Link from 'next/link'
+import { HeaderBar } from './components/header';
 const axios = require('axios');
 
 export default function Home({ data }) {
@@ -11,64 +12,108 @@ export default function Home({ data }) {
   )
 }
 
+const getDate = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const day = () => {
+        if (date.getDate().toString() === "1" && month - 1 === (2 || 4 || 6 || 9 || 11)) {
+            return 30; 
+        } else if (date.getDate().toString() === "1" && month - 1 === (1 || 3 || 5 || 7 || 8 || 10 || 12)) {
+            return 31;
+        } else {
+            return date.getDate() - 1
+        }
+    }
+    const month = () => {
+        if (date.getMonth() <= 9) {
+            return `0${date.getMonth() + 1}`
+        } else return date.getMonth() + 1
+    }
+    return `${year}-${month()}-${day()}`
+}
+
 export const getStaticProps = async () => {
-  const options = {
-      method: 'GET',
-      url: 'https://covid-19-statistics.p.rapidapi.com/regions',
-      headers: {
-        'x-rapidapi-host': 'covid-19-statistics.p.rapidapi.com',
-        'x-rapidapi-key': process.env.RAPID_API_KEY
-      }
-  };
-  const result = await axios.request(options);
-  const dataFromResult = result.data.data;
-  let data = [];
+    const dateForOptions = getDate();
+    const options = {
+        method: 'GET',
+        url: 'https://covid-19-statistics.p.rapidapi.com/regions',
+        headers: {
+            'x-rapidapi-host': 'covid-19-statistics.p.rapidapi.com',
+            'x-rapidapi-key': "0a0ac6083dmshd4b9d1a80ab8e97p1c323ejsn671ecebffd29"
+        }
+    };
+    const result = await axios.request(options);
+    const dataFromResult = result.data.data;
+    let data = [];
 
-  await Promise.all(dataFromResult.map(async (item, index) => {
-      const options = {
-              method: 'GET',
-              url: 'https://covid-19-statistics.p.rapidapi.com/reports',
-              params: {
-                iso: item.iso,
-                region_name: item.name,
-                date: '2022-01-14'
-              },
-              headers: {
-                'x-rapidapi-host': 'covid-19-statistics.p.rapidapi.com',
-                'x-rapidapi-key': process.env.RAPID_API_KEY
-              }
-            };
-  
-          const result = await axios.request(options);
-          const dataV = result.data.data[0];
-          let region;
-  
-          if (dataV?.region === undefined) {
-              region = item.name;
-          } else {
-              region = dataV.region;
-          }
-  
-          if (dataV === undefined) return;
-  
-          const dataStructure = {
-              "country_name": item.name,
-              "cases": dataV.confirmed,
-              "deaths": dataV.deaths,
-              "recovered": dataV.recovered,
-              "active": dataV.active,
-              "fatality_rate": dataV.fatality_rate,
-              "date": dataV.date,
-              "last_update": dataV.last_update,
-              "region": region
-          }
-          
-          data = [
-              ...data,
-              { dataStructure }
-          ]
-  }));
+    await Promise.all(dataFromResult.map(async (item, index) => {
+        const options = {
+            method: 'GET',
+            url: 'https://covid-19-statistics.p.rapidapi.com/reports',
+            params: {
+            iso: item.iso,
+            region_name: item.name,
+            date: dateForOptions
+            },
+            headers: {
+            'x-rapidapi-host': 'covid-19-statistics.p.rapidapi.com',
+            'x-rapidapi-key': "0a0ac6083dmshd4b9d1a80ab8e97p1c323ejsn671ecebffd29"
+            }
+        };
 
+        const result = await axios.request(options);
+        const dataV = result.data.data;
+        let region;
+
+        if (dataV[0]?.region === undefined) {
+        region = item.name;
+        } else {
+        region = dataV[0].region;
+        }
+
+        if (dataV[0] === undefined) return;
+
+        let confirmed = { "confirmed": 0 };
+        let deaths = { "deaths": 0 };
+        let recovered = { "recovered": 0 };
+        let active = { "active": 0 };
+        let fatality_rate;
+
+        dataV.map(item => {
+            confirmed = { "confirmed": item.confirmed + confirmed.confirmed }
+            deaths = { "deaths": item.deaths + deaths.deaths }
+            recovered = { "recovered": item.recovered + recovered.recovered }
+            active = { "active": item.active + active.active }
+            const getFatalityRate = () => {
+                let rateV = (deaths.deaths / confirmed.confirmed) * 100;
+                const rate = { "fatality_rate": rateV }
+                return rate;
+            }
+            fatality_rate = getFatalityRate();
+        })
+
+        const dataStructure = {
+        "country_name": item.name,
+        "cases": confirmed.confirmed,
+        "deaths": deaths.deaths,
+        "recovered": recovered.recovered,
+        "active": active.active,
+        "fatality_rate": fatality_rate.fatality_rate,
+        "date": dataV[0].date,
+        "last_update": dataV[0].last_update,
+        "region": region,
+        "iso": item.iso
+        }
+        
+        /* console.log(`Country: ${dataStructure.country_name}
+    Confirmed: ${dataStructure.cases}
+    `); */
+
+        data = [
+        ...data,
+        { dataStructure }
+        ]
+    }));
 
   return {
       props: { 
@@ -79,35 +124,26 @@ export const getStaticProps = async () => {
 }
 
 function ActiveCases({ data }) {
-  console.log('inside active cases')
+    const addComma = (x) => {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
   return (
-      <div className={styles.AllData}>
+      <div>
           <h1 className={styles.Header}>Active Cases</h1>
           <div>
             { data.map((item, index) => {
                 return (
                     <span className={styles.DataWrapper} key={index}>
                         <p>Country: {item.dataStructure.country_name} </p>
-                        <p>Cases: {item.dataStructure.cases}</p>
-                        <p>Deaths: {item.dataStructure.deaths}</p>
+                        <p>Cases: {addComma(item.dataStructure.cases)}</p>
+                        <p>Deaths: {addComma(item.dataStructure.deaths)}</p>
                         <p>Data from {item.dataStructure.last_update}</p>
-                        <p className={styles.LinkStyle}><Link href={{pathname: `/${item.dataStructure.country_name}`}}>Get more data</Link></p>
+                        <p className={styles.LinkStyle}><Link href={{pathname: `/${item.dataStructure.iso}`}}>Get more data</Link></p>
                     </span>
                 )
             }) }
           </div>
       </div>
-  )
-}
-
-const HeaderBar = () => {
-
-  return (
-    <div className={styles.HeaderBar}>
-      <h4 className={styles.HeaderItem}><Link href={{pathname: `/About`}}>About</Link></h4>
-      <h4 className={styles.HeaderItem}><Link href={{pathname: `/More-Data`}}>More Data</Link></h4>
-      <h4 className={styles.HeaderItem}><Link href={{pathname: `/Developer-Credits`}}>{"Developer & Credits"}</Link></h4>
-    </div>
   )
 }
 
