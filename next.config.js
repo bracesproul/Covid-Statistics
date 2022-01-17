211ad1fecb4807f96ef39c97ea14fbc318e14cd9
Brace Sproul <braceasproul@gmail.com>
2022-01-17 22:01:43 +0000

    a few changes to how env variables are stored

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
+require('dotenv').config()
+
 module.exports = {
   reactStrictMode: true,
+  env: {
+    MONGO_URI: process.env.MONGO_URI,
+    RAPID_API_KEY: process.env.RAPID_API_KEY,
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+  }
 }
