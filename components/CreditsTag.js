cf658adb5621aaa92decda31c15488e256814d7e
Brace Sproul <braceasproul@gmail.com>
2022-02-04 18:42:13 +0000

    added rel='noreferrer' to fix sec risk

--- components/CreditsTag.js
+++ components/CreditsTag.js
@@ -1,7 +1,7 @@
 export const CreditsTag = () => {
     return (
         <>
-            <p className="credits-text">Data sourced from <strong className="credits-text-link"><a href="https://ourworldindata.org/coronavirus" target="_blank">Our World in Data</a></strong></p>
+            <p className="credits-text">Data sourced from <strong className="credits-text-link"><a rel="noreferrer" href="https://ourworldindata.org/coronavirus" target="_blank">Our World in Data</a></strong></p>
         </>
     )
 }
