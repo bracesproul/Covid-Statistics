86654eb74ce3f40b9daf683fbbcf470c418852b9
Brace Sproul <braceasproul@gmail.com>
2022-02-04 19:05:51 +0000

    switched chart to pull from component instead of function inside file

--- pages/[id].js
+++ pages/[id].js
@@ -8,6 +8,7 @@ import { currentDate } from '../components/dates';
 import { AreaChart, XAxis, YAxis, Tooltip, Area, ResponsiveContainer } from 'recharts';
 import iso_and_country_list from '../components/iso_and_country_list';
 import { CreditsTag } from '../components/CreditsTag';
+import { Chart } from '../components/Chart';
 
 let globalQuerryId;
 export default function Home({ data }) {
@@ -23,7 +24,7 @@ export default function Home({ data }) {
             <div className="card-container">
                 <CountryData data={data} />
             </div>
-            <TestChart dataHistory={dataHistory} />
+            <Chart dataHistory={dataHistory} globalQuerryId={globalQuerryId} />
             <CreditsTag />
         </div>
     )
@@ -198,69 +199,3 @@ const CountryData = ({ data }) => {
         </article>
     )
 }
-
-
-
-const TestChart = ({ dataHistory }) => {
-
-    if (!dataHistory) {
-        console.log("No chart data for this country");
-        console.log("QUERRY -", globalQuerryId);
-        return null;
-    }
-
-    const data = dataHistory.map(it => {
-        let new_cases = Number(it.new_cases)
-        let new_deaths = Number(it.new_deaths)
-        let total_cases = Number(it.total_cases)
-        let total_deaths = Number(it.total_deaths)
-        const dateNum = Number(it.date_number);
-
-        const deathsArr = it.new_deaths.split(',');
-        const casesArr = it.new_cases.split(',');
-
-        if (deathsArr[0].includes('-')) {
-            new_deaths = 0;
-        };
-        if (casesArr[0].includes('-')) {
-            new_cases = 0;
-        };
-        return {
-            new_cases: new_cases,
-            new_deaths: new_deaths,
-            total_cases: total_cases,
-            total_deaths: total_deaths,
-            date: it.date,
-            date_number: dateNum
-        }
-    }) 
-    return (
-        <div className="charts-container">
-            <div className="chart">
-            <h1 className="country-title">Cases*</h1>
-            <ResponsiveContainer width={800} height={350} >
-                <AreaChart data={data}
-                margin={{ top: 20, right: 30, left: 0, bottom: 0 }}>
-                <XAxis dataKey="date" />
-                <YAxis />
-                <Tooltip dataKey="date" />
-                <Area type="monotone" dataKey="new_cases" stroke="#8884d8" fill="#8884d8" />
-                </AreaChart>
-            </ResponsiveContainer>
-            </div>
-            <div className="chart">
-            <h1 className="country-title">Deaths*</h1>
-            <ResponsiveContainer width={800} height={350} >
-                <AreaChart data={data}
-                margin={{ top: 20, right: 30, left: 0, bottom: 0 }}>
-                <XAxis dataKey="date" />
-                <YAxis />
-                <Tooltip dataKey="date" />
-                <Area type="monotone" dataKey="new_deaths" stroke="#8884d8" fill="#8884d8" />
-                </AreaChart>
-            </ResponsiveContainer>
-            </div>
-        </div>
-    )
-}
-
--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ import { CreditsTag } from '../components/CreditsTag';
 import { useState } from 'react';
 import { readData } from '../components/readDataHistory';
 import { AreaChart, XAxis, YAxis, Tooltip, Area, ResponsiveContainer } from 'recharts';
+import { Chart } from '../components/Chart';
 
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
@@ -240,72 +241,11 @@ function ActiveCases({ data }) {
   )
 }
 
-
-const WorldDataChart = ({ dataHistory }) => {
-
-    const data = dataHistory.map(it => {
-        let new_cases = Number(it.new_cases)
-        let new_deaths = Number(it.new_deaths)
-        let total_cases = Number(it.total_cases)
-        let total_deaths = Number(it.total_deaths)
-        const dateNum = Number(it.date_number);
-
-        const deathsArr = it.new_deaths.split(',');
-        const casesArr = it.new_cases.split(',');
-
-        if (deathsArr[0].includes('-')) {
-            new_deaths = 0;
-        };
-        if (casesArr[0].includes('-')) {
-            new_cases = 0;
-        };
-        return {
-            new_cases: new_cases,
-            new_deaths: new_deaths,
-            total_cases: total_cases,
-            total_deaths: total_deaths,
-            date: it.date,
-            date_number: dateNum
-        }
-    }) 
-
-    return (
-        <div className="charts-container">
-            <div className="chart">
-            <h1 className="country-title">Cases</h1>
-            <ResponsiveContainer width={800} height={350} >
-                <AreaChart data={data}
-                margin={{ top: 20, right: 30, left: 0, bottom: 0 }}>
-                <XAxis dataKey="date" />
-                <YAxis />
-                <Tooltip dataKey="date" />
-                <Area type="monotone" dataKey="new_cases" stroke="#8884d8" fill="#8884d8" />
-                </AreaChart>
-            </ResponsiveContainer>
-            </div>
-            <div className="chart">
-            <h1 className="country-title">Deaths</h1>
-            <ResponsiveContainer width={800} height={350} >
-                <AreaChart data={data}
-                margin={{ top: 20, right: 30, left: 0, bottom: 0 }}>
-                <XAxis dataKey="date" />
-                <YAxis />
-                <Tooltip dataKey="date" />
-                <Area type="monotone" dataKey="new_deaths" stroke="#8884d8" fill="#8884d8" />
-                </AreaChart>
-            </ResponsiveContainer>
-            </div>
-        </div>
-    )
-}
-
 const FinalChart = ({ dataHistory }) => {
     return (
         <>
         <h1 className={styles.Header} >Worldwide Data</h1>
-        <WorldDataChart dataHistory={dataHistory} />
+        <Chart dataHistory={dataHistory} globalQuerryId="OWID_WRL" />
         </>
-
-        
     )
 }
